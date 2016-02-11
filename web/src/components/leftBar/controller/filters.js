a60ec4cf0777a0e0cc5d7e93a504b828613b32a6
Nick482 <kolya.bandura@gmail.com>
2016-02-11 11:21:23 +0200

    refactor

--- web/src/components/leftBar/controller/filters.js
+++ web/src/components/leftBar/controller/filters.js
@@ -1,38 +1,23 @@
 angular.module('filters', []).controller('filters', function($scope, variableBuffer){
-    $scope.excludedFilters = ["id", "name", "group", "subgroup", "image", "code", "remaining", "createdAt", "updatedAt"];
     $scope.showPrice = false;
 
     $scope.$on('setFilters', function(setFilters, data){
-        $scope.filterTypes = {};
         var received = data.data,
             priceTemp = [];
         $scope.received = received;
-        console.log(received);
-        var tempArray = [];
-        for (var i = 0; i < received.length; i++) {
-            if (tempArray.indexOf(Object.keys(received[i])) == -1 &&
-                $scope.excludedFilters.indexOf(Object.keys(received[i])) == -1){
-                    tempArray.push(Object.keys(received[i]))
-                }
-        }
-        for (i = 0; i < tempArray.length; i++) {
-            for(var j = 0; j < tempArray[i].length; j++) {
-                if ($scope.excludedFilters.indexOf(tempArray[i][j]) == -1){
-                    $scope.filterTypes[tempArray[i][j]] = [];
-                }
+        $scope.filterTypes = received.reduce(
+            function(ob, element){
+                ob.price.push(element.price);
+                ob.maker.push(element.maker);
+                return ob;
+            }, {price: [], maker: []}
+        );
+        $scope.filterTypes.maker = $scope.filterTypes.maker.filter(
+            function(element, index, self){
+                return self.indexOf(element) == index;
             }
-        }
+        );
 
-        for(i = 0; i < Object.keys($scope.filterTypes).length; i++){
-            for(j = 0; j < received.length; j++){
-                for(var k = 0; k < Object.keys(received[j]).length; k++) {
-                    if (Object.keys($scope.filterTypes)[i] == Object.keys(received[j])[k] &&
-                        $scope.filterTypes[Object.keys($scope.filterTypes)[i]].indexOf(received[j][Object.keys(received[j])[k]]) == -1) {
-                        $scope.filterTypes[Object.keys($scope.filterTypes)[i]].push(received[j][Object.keys(received[j])[k]])
-                    }
-                }
-            }
-        }
     $scope.filterTypes.price.sort();
     priceTemp = [$scope.filterTypes.price[0], $scope.filterTypes.price[$scope.filterTypes.price.length - 1]];
     delete $scope.filterTypes.price;
