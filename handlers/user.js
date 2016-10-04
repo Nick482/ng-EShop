a15fad6ec24c183223b12870e1afbb2238882dec
Nick482 <kolya.bandura@gmail.com>
2016-10-04 14:08:17 +0300

    user handler

--- handlers/user.js
+++ handlers/user.js
@@ -1,1 +1,58 @@
 var models = require('../models');
+var User = models.User;
+var Order = models.Order;
+
+function add(req, res, next) {
+	var user = new User(req.body);
+
+	user.save(function(err, user){
+		if(err) {
+			return next(err);
+		}
+		res.status(201).send('Success');
+	});
+}
+
+function update(req, res, next) {
+	User.findByIdAndUpdate(req.body_id, req.body, function(err, user){
+		if(err) {
+			return next(err);
+		}
+		res.status(200).send("Success");
+	});
+}
+
+function get(req, res, next) {
+	User.findById(req.params.id, function(err, user){
+		if(err) {
+			return next(err)
+		}
+		res.status(200).send(user);
+	})
+}
+
+function getOrders(req, res, next){
+	Order.find({user: req.params.id}).populate('products').exec(function(err, orders){
+		if(err) {
+			return next(err);
+		}
+		res.status(200).send(orders);
+	});
+}
+
+function remove(req, res, next) {
+	User.findByIdAndRemove(req.params.id, function(err, user){
+		if(err) {
+			return next(err);
+		}
+		res.status(200).send('Success');
+	})
+}
+
+module.exports = {
+	add: add,
+	update: update,
+	get: get,
+	getOrders: getOrders,
+	remove: remove
+}
