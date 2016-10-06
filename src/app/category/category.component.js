7ad42b681d8be8ddce1860dc34e86a3ada9d6ebd
Nick482 <kolya.bandura@gmail.com>
2016-10-06 16:35:09 +0300

    category

--- src/app/category/category.component.js
+++ src/app/category/category.component.js
@@ -3,6 +3,7 @@
 	.module('app')
 	.component('category', {
 		templateUrl: 'app/category/category.html',
+		bindings: {subcategories: '<'},
 		controller: 'categoryCtrl',
 		controllerAs: 'vm'
 	});
